28a23ccc388611875f232d7b9ac9793a043bc5df
averak <abe12@mccc.jp>
2021-08-16 23:14:01 +0900

    [feat] unit / integration test interface

---
+++ app/src/test/java/dev/abelab/rippy/annotation/UnitTest.java
@@ -0,0 +1,15 @@
+package dev.abelab.rippy.annotation;
+
+import java.lang.annotation.*;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * Unit Test Interface
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("UnitTest")
+@Inherited
+public @interface UnitTest {
+}
