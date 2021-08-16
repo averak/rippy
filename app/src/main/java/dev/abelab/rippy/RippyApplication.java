863efdea714c8969e563b8a69eadfb6484a18bf6
averak <abe12@mccc.jp>
2021-08-16 09:37:57 +0900

    [feat] rippy application entory point

---
+++ app/src/main/java/dev/abelab/rippy/RippyApplication.java
@@ -0,0 +1,11 @@
+package dev.abelab.rippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RippyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RippyApplication.class, args);
+    }
+}
---
+++ app/src/test/java/dev/abelab/rippy/RippyApplicationTest.java
@@ -0,0 +1,7 @@
+package dev.abelab.rippy;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RippyApplicationTest {
+}
