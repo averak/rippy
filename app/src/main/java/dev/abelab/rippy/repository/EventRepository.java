576da084275ee05a471af082f9da18876c5e3100
averak <abe12@mccc.jp>
2021-08-28 04:33:53 +0900

    [feat] create event repository

---
+++ app/src/main/java/dev/abelab/rippy/repository/EventRepository.java
@@ -0,0 +1,40 @@
+package dev.abelab.rippy.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import dev.abelab.rippy.db.entity.Event;
+import dev.abelab.rippy.db.entity.EventExample;
+import dev.abelab.rippy.db.mapper.EventMapper;
+
+@RequiredArgsConstructor
+@Repository
+public class EventRepository {
+
+    private final EventMapper eventMapper;
+
+    /**
+     * イベント一覧を取得
+     *
+     * @return イベント一覧
+     */
+    public List<Event> selectAll() {
+        final var eventExample = new EventExample();
+        eventExample.setOrderByClause("updated_at desc");
+        return this.eventMapper.selectByExample(eventExample);
+    }
+
+    /**
+     * イベントを作成
+     *
+     * @param event イベント
+     *
+     * @return イベントID
+     */
+    public int insert(final Event event) {
+        return this.eventMapper.insertSelective(event);
+    }
+
+}
